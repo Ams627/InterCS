bdc8319735fcd4587ad1fc2a98be7248b6b46c82
Adrian Sims <asims@parkeon.com>
2018-03-12 15:59:49 +0000

    Add project files.

---
+++ InterCS/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterCS
+{
+    class TestAttribute : System.Attribute
+    {
+
+    }
+
+    [Test] class X
+    {
+        public string Name { get; set; }
+    }
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                var list = new[] { "Apple", "Pear", "Banana", "Grape", "Pineapple", "Apple", "Kiwi", "lemon", "lime" };
+
+                // what is ForEach? Why do we call ToList()?
+                list.ToList().ForEach(x => Console.WriteLine($"fruit is {x}"));
+
+                // What is action?
+                Action<string> a = (x) => Console.WriteLine($"fruit is {x}");
+                list.ToList().Sort();
+                list.ToList().ForEach(a);
+
+                Console.WriteLine("--------");
+
+                // How do we do sort the fruit list in reverse?
+                var fruitList = list.ToList();
+                fruitList.Sort(/* what goes here */);
+                fruitList.ForEach(a);
+
+                // print the name and count of all duplicates in the fruit list:
+                fruitList.GroupBy(x => x).Where(y => y.Count() > 1).Select(z => new { z.Key, Cnt = z.Count() }).ToList().ForEach(q => Console.WriteLine($"Key ={q.Key} count: {q.Cnt}"));
+            }
+            catch (Exception ex)
+            {
+                var codeBase = System.Reflection.Assembly.GetExecutingAssembly().CodeBase;
+                var progname = Path.GetFileNameWithoutExtension(codeBase);
+                Console.Error.WriteLine(progname + ": Error: " + ex.Message);
+            }
+
+        }
+    }
+}
---
+++ gitmehappy.cmd
@@ -0,0 +1,8 @@
+rem echo off
+git init
+git add InterCS/Program.cs
+git add InterCS/App.config
+git add InterCS/Properties/AssemblyInfo.cs
+git add InterCS/InterCS.csproj
+git add InterCS.sln
+git add .gitignore
